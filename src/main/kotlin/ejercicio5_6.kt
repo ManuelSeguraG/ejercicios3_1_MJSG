377c62900c387cfca66c50032d0235d64e49d45a
ManuelSeguraG <mjsegura@iesrafaelalberti.es>
2021-10-20 20:08:32 +0200

    Commit 1 - Actividad 5.6 hecha - Archivo Actividad 5.7 creada

--- src/main/kotlin/ejercicio5_6.kt
+++ src/main/kotlin/ejercicio5_6.kt
@@ -1,7 +1,23 @@
-class rectangulo(var base: Double){
+class Rectangulo(var base: Double, var altura: Double) {
 
+    override fun toString(): String {
+        return super.toString()
+    }
+
+    fun calcularArea(): Double = base * altura
+
+    fun calcularPerimetro(): Double = 2 * base + 2 * altura
 }
 
 fun main() {
-    println("Hello World!")
+    var rectangulo1 = Rectangulo(1.5,2.0)
+    var rectangulo2 = Rectangulo(1.0,5.5)
+    var rectangulo3 = Rectangulo(4.5,3.0)
+
+    println("Area rectangulo 1: ${rectangulo1.calcularArea()}")
+    println("Area rectangulo 2: ${rectangulo2.calcularArea()}")
+    println("Area rectangulo 3: ${rectangulo3.calcularArea()}")
+    println("Perimetro rectangulo 1: ${rectangulo1.calcularPerimetro()}")
+    println("Perimetro rectangulo 2: ${rectangulo2.calcularPerimetro()}")
+    println("Perimetro rectangulo 3: ${rectangulo3.calcularPerimetro()}")
 }
---
+++ src/main/kotlin/ejercicio5_7.kt
@@ -0,0 +1,5 @@
+class Coche
+
+fun main(){
+
+}
