f2f71fd8638529aa08fc24f8b417647dc044c58f
ManuelSeguraG <mjsegura@iesrafaelalberti.es>
2021-10-20 20:44:20 +0200

    Commit 2 - Actividad 5.7 hecha - Archivo Actividad 5.8 creada

--- src/main/kotlin/ejercicio5_6.kt
+++ src/main/kotlin/ejercicio5_6.kt
@@ -10,9 +10,9 @@ class Rectangulo(var base: Double, var altura: Double) {
 }
 
 fun main() {
-    var rectangulo1 = Rectangulo(1.5,2.0)
-    var rectangulo2 = Rectangulo(1.0,5.5)
-    var rectangulo3 = Rectangulo(4.5,3.0)
+    var rectangulo1 = Rectangulo(1.5, 2.0)
+    var rectangulo2 = Rectangulo(1.0, 5.5)
+    var rectangulo3 = Rectangulo(4.5, 3.0)
 
     println("Area rectangulo 1: ${rectangulo1.calcularArea()}")
     println("Area rectangulo 2: ${rectangulo2.calcularArea()}")
--- src/main/kotlin/ejercicio5_7.kt
+++ src/main/kotlin/ejercicio5_7.kt
@@ -1,7 +1,31 @@
-class Coche(){
+class Coche(
+    var colorCoche: String,
+    var marcaCoche: String,
+    var modeloCoche: String,
+    var numeroCaballosCoche: Int,
+    var numeroPuertasCoche: Int,
+    var matriculaCoche: String
+) {
+
+    override fun toString(): String {
+        return "El $marcaCoche $modeloCoche color $colorCoche matricula $matriculaCoche cuenta con $numeroCaballosCoche caballos de potencia y $numeroPuertasCoche puertas en el coche"
+    }
 
 }
 
-fun main(){
+fun main() {
+    var coche1 = Coche("Negro", "Fiat", "Panda", 15, 4, "3001MJS")
+    var coche2 = Coche("#69F420", "Mercedab", "MLGLA", 69, 7, "6942MLG")
+    var coche3 = Coche("Rojo con lunares blancos", "Fiah", "Cascaoh", 2, 2, "0010LMH")
 
+    println(coche1)
+    println(coche2)
+    println(coche3)
+    println("")
+    coche1.colorCoche = "Blanco"
+    coche2.colorCoche = "#F42069"
+    coche3.colorCoche = "Isabel Pantoja"
+    println(coche1)
+    println(coche2)
+    println(coche3)
 }
